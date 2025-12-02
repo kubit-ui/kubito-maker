2bb34b11aab5f5ff30c80f726bcedfe473c7a6d3
n128691-sanhq_sangroup <hector.arce@gruposantander.com>
2025-12-02 12:17:27 +0100

    feat(project): add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,32 @@
-import ReactDOM from "react-dom/client";
-import { Toaster } from "react-hot-toast";
-import KubitoEditor from "./KubitoEditor.tsx";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import KubitoEditor from './KubitoEditor.tsx';
+import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Root element not found");
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
 
 ReactDOM.createRoot(rootElement).render(
   <>
+    <Analytics />
     <KubitoEditor />
     <Toaster
       position="bottom-right"
       toastOptions={{
         duration: 2000,
         style: {
-          background: "#363636",
-          color: "#fff",
-          borderRadius: "8px",
+          background: '#363636',
+          color: '#fff',
+          borderRadius: '8px',
         },
         success: {
           iconTheme: {
-            primary: "#10b981",
-            secondary: "#fff",
+            primary: '#10b981',
+            secondary: '#fff',
           },
         },
       }}
     />
-  </>,
+  </>
 );
